250600b58567265432f318707eb8854c79ddc8e1
kivid <1695949332@qq.com>
2021-04-16 00:38:14 +0800

    add echarts npm

--- src/components/Show.vue
+++ src/components/Show.vue
@@ -1,13 +1,86 @@
 <template>
-  <div class="view-content">
-    <div id="myChart" :style="{width: '300px', height: '300px'}"></div>
-  </div>
+<dv-full-screen-container>
+<dv-border-box-8 :color="['black', 'green']" backgroundColor="pink" >
+
+<div class="container" id="show">
+	<div class="row clearfix">
+		<div class="col-md-4 column">
+      
+		</div>
+		<div class="col-md-4 column">
+		</div>
+		<div class="col-md-4 column">
+		</div>
+	</div>
+	<div class="row clearfix">
+		<div class="col-md-6 column">
+		</div>
+		<div class="col-md-6 column">
+		</div>
+	</div>
+	<div class="row clearfix">
+		<div class="col-md-12 column">
+		</div>
+	</div>
+</div>
+</dv-border-box-8>
+</dv-full-screen-container>
 </template>
 
 <script>
-  export default {
+const option1 = {
+  title: {
+    text: '周销售额趋势'
+  },
+  xAxis: {
+    name: '第一周',
+    data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+  },
+  yAxis: {
+    name: '销售额',
+    data: 'value'
+  },
+  series: [
+    {
+      data: [1200, 2230, 1900, 2100, 3500, 4200, 3985],
+      type: 'bar',
+      gradient: {
+        color: ['#37a2da', '#67e0e3']
+      }
+    }
+  ]
+}
 
-  }
+const option2 = {
+  title: {
+    text: '周销售额趋势'
+  },
+  xAxis: {
+    name: '第二周',
+    data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+  },
+  yAxis: {
+    name: '销售额',
+    data: 'value'
+  },
+  series: [
+    {
+      data: [2339, 1899, 2118, 1790, 3265, 4465, 3996],
+      type: 'bar',
+      gradient: {
+        color: ['#67e0e3', '#37a2da']
+      }
+    }
+  ]
+}
+
+export default [option1, option2]
+  // export default {
+
+  // }
 </script>
 <style  scoped>
+/* div{
+  background: red;
+} */
 </style>
--- src/main.js
+++ src/main.js
@@ -3,13 +3,34 @@ import Vue from 'vue';
 import Element from 'element-ui';
 Vue.use(Element, { size: 'small', zIndex: 3000 });
 import 'element-ui/lib/theme-chalk/index.css'
+
 import '../static/css/bootstrap.min.css'
 import VueRouterN from 'vue-router'
 Vue.use(VueRouterN);
+// import Charts from '@jiaminghi/charts'
 
+// const container = document.getElementById('show')
+
+// const myChart = new Charts(container)
+// /**
+//  * @description 设置图表配置
+//  * @param {Object} option 图表配置
+//  * @param {Boolean} animationEnd 执行animationEnd
+//  * @return {Undefined} 无返回值
+//  */
+//  Charts.prototype.setOption = function (option, animationEnd = false) {
+//   //...
+//   animationEnd = false;
+// }
+
+
+// 将自动注册所有组件为全局组件
+import dataV from '@jiaminghi/data-view'
+Vue.use(dataV)
 //设置路由
 import start from './components/Home.vue'
 import train from './components/Train.vue'
+import show from './components/Show.vue'
 let router = new VueRouterN({
   routes:[
     {
@@ -19,6 +40,10 @@ let router = new VueRouterN({
     {
       path: '/train',
       component: train
+    },
+    {
+      path: '/show',
+      component: show
     }
   ]
 })
